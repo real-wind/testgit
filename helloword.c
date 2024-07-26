4d36d790f5aa4dd37deaaf99bc77b33353b0c4c6
real-wind <stuzhengsc@163.com>
2024-07-26 23:37:22 +0800

    master

---
+++ helloword.c
@@ -0,0 +1,16 @@
+#include <stdio.h>  // /usr/include/stdio.h
+
+int main()
+{
+    printf("hello");
+    printf("hello");
+
+    printf("hello");
+    printf("hello");
+
+
+
+
+    printf("hello");
+    return 0;
+}
